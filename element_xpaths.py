36bd78cd393afd89a789cfd94e7cff13060f3ae2
gagansth <49076158+gagansth@users.noreply.github.com>
2024-07-30 21:36:05 +0545

    Create element_xpaths.py

---
+++ element_xpaths.py
@@ -0,0 +1,9 @@
+JOBS_ELM = "//nav//*[contains(text(),'Jobs')]"
+JOB_TITLE_ELM = "//section[contains(@class,'search-bar')]//input[@placeholder='Search job titles or companies']"
+SEARCH_RESULTS_UL_ELM = "jobs-search__results-list"
+JOB_LOCATION_ELM = "//section[contains(@class,'search-bar')]//input[@name='location']"
+JD_CARD_ELM = "//section/div[@class='details-pane__content details-pane__content--show']"
+JD_CARD_JOB_TITLE_ELM = "//h2[contains(@class,'top-card-layout__title')]" #get element from JD_CARD_ELM
+JD_CARD_JOB_SENIORITY_LEVEL_ELM = "//h3[contains(text(),'Seniority level')]/following-sibling::span"
+JD_CARD_EMP_TYPE_LEVEL_ELM = "//h3[contains(text(),'Employment type')]/following-sibling::span"
+SEE_MORE_JOBS_BTN_ELM = "//button[contains(text(),'See more jobs')]"
