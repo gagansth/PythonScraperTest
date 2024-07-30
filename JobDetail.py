54fb40c57d57ba82519aa0776d85a6f9cb143186
gagansth <49076158+gagansth@users.noreply.github.com>
2024-07-30 21:36:27 +0545

    Create JobDetail.py

---
+++ JobDetail.py
@@ -0,0 +1,8 @@
+class JobDetail:
+        job_title = ""
+        company_name = ""
+        company_location = ""
+        pay_range = ""
+        job_level = ""
+        employment_type = ""
+        job_posted_datetime = ""
