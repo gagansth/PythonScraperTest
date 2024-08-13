5a5275f07d647d729b201b8b6e06c2c384715324
gagansth <49076158+gagansth@users.noreply.github.com>
2024-08-13 18:16:54 +0545

    Fix company_name element path. Write remaining data to output file

--- requeststest.py
+++ requeststest.py
@@ -6,10 +6,10 @@ from datetime import datetime, timedelta
 import csv
 import re
 import argparse
-import concurrent.futures
+import traceback
 
 #custom imports
-#from JobDetail import JobDetail
+# from JobDetail import JobDetail
 
 #Ignore warnings shown while making api requests
 warnings.filterwarnings('ignore')
@@ -26,6 +26,7 @@ job_location = args.job_location
 job_details = []
 
 class JobDetail:
+        job_id = ""
         job_title = ""
         company_name = ""
         company_location = ""
@@ -33,6 +34,7 @@ class JobDetail:
         job_level = ""
         employment_type = ""
         job_posted_datetime = ""
+        industry = ""
 
 def parse_relative_date(relative_str):
     current_date = datetime.now()
@@ -59,43 +61,50 @@ def parse_relative_date(relative_str):
     return (current_date - delta).isoformat(sep=" ", timespec="seconds")
 
 def parse_job_details(job_id, attempt = 1):
-    job_url = f"https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{job_id}"
-    if attempt > 10: 
-        print(f"Could not get proper response from api even after 10 attempts!!! Skipping this job_id => {job_id}")
-        return
-    
-    job_detail_response = requests.get(job_url, verify=False)
-    #print(f"Getting job details attempt - {attempt} and got response - {job_detail_response.status_code}")
-    if job_detail_response.status_code == 200:
-        
-        job_details_soup = BeautifulSoup(job_detail_response.text, "html.parser")
-        jd_job_title_tag = job_details_soup.find("h2", {"class": "top-card-layout__title font-sans text-lg papabear:text-xl font-bold leading-open text-color-text mb-0 topcard__title"})
-        jd_company_name_tag = job_details_soup.find("a", {"class": "topcard__org-name-link topcard__flavor--black-link"})
-        jd_company_location_tag = job_details_soup.find("span", {"class" : "topcard__flavor topcard__flavor--bullet"})
-        jd_job_criteria_li_tags = job_details_soup.find("ul", {"class", "description__job-criteria-list"}).find_all("li")
-        jd_pay_range_tag = job_details_soup.find("div", {"class", "salary compensation__salary"})
-        # jd_job_posted_datetime_tag = job_details_soup.find("span", {"class", "posted-time-ago__text posted-time-ago__text--new topcard__flavor--metadata"})
-        jd_job_posted_datetime_tag = jd_company_location_tag.find_parent("div").find_next_sibling("div").findChild("span")
-        
-        #Adding data to JobDetail object
-        job_detail = JobDetail()
-        job_detail.job_title = jd_job_title_tag.text.strip()
-        job_detail.company_name = jd_company_name_tag.text.strip()
-        job_detail.company_location = jd_company_location_tag.text.strip()
-        job_detail.job_level = jd_job_criteria_li_tags[0].find("span").text.strip()
-        job_detail.employment_type = jd_job_criteria_li_tags[1].find("span").text.strip()
-        try:
-            job_detail.pay_range = jd_pay_range_tag.text.strip()
-        except:
-            job_detail.pay_range = ""
-        job_detail.job_posted_datetime = parse_relative_date(jd_job_posted_datetime_tag.text.strip())
+    try:
+        job_url = f"https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{job_id}"
+        if attempt > 10: 
+            print(f"Could not get proper response from api even after 10 attempts!!! Skipping this job_id => {job_id}")
+            return
 
-        job_details.append(job_detail)
-        return
-    else:
-        attempt += 1
-    #Call function with same job id for another attempt    
-    parse_job_details(job_id, attempt)
+        job_detail_response = requests.get(job_url, verify=False)
+        #print(f"Getting job details attempt - {attempt} and got response - {job_detail_response.status_code}")
+        if job_detail_response.status_code == 200:
+
+            job_details_soup = BeautifulSoup(job_detail_response.text, "html.parser")
+            jd_job_title_tag = job_details_soup.find("h2", {"class": "top-card-layout__title font-sans text-lg papabear:text-xl font-bold leading-open text-color-text mb-0 topcard__title"})
+            jd_company_location_tag = job_details_soup.find("span", {"class" : "topcard__flavor topcard__flavor--bullet"})
+            # jd_company_name_tag = job_details_soup.find("a", {"class": "topcard__org-name-link topcard__flavor--black-link"})
+            jd_company_name_tag = jd_company_location_tag.find_previous_sibling("span")
+            jd_job_criteria_li_tags = job_details_soup.find("ul", {"class", "description__job-criteria-list"}).find_all("li")
+            jd_pay_range_tag = job_details_soup.find("div", {"class", "salary compensation__salary"})
+            # jd_job_posted_datetime_tag = job_details_soup.find("span", {"class", "posted-time-ago__text posted-time-ago__text--new topcard__flavor--metadata"})
+            jd_job_posted_datetime_tag = jd_company_location_tag.find_parent("div").find_next_sibling("div").findChild("span")
+
+            #Adding data to JobDetail object
+            job_detail = JobDetail()
+            job_detail.job_id = job_id
+            job_detail.job_title = jd_job_title_tag.text.strip()
+            job_detail.company_name = jd_company_name_tag.text.strip()
+            job_detail.company_location = jd_company_location_tag.text.strip()
+            job_detail.job_level = jd_job_criteria_li_tags[0].find("span").text.strip()
+            job_detail.employment_type = jd_job_criteria_li_tags[1].find("span").text.strip()
+            job_detail.industry = jd_job_criteria_li_tags[3].find("span").text.strip()
+            try:
+                job_detail.pay_range = jd_pay_range_tag.text.strip()
+            except:
+                job_detail.pay_range = ""
+            job_detail.job_posted_datetime = parse_relative_date(jd_job_posted_datetime_tag.text.strip())
+
+            job_details.append(job_detail)
+            return
+        else:
+            attempt += 1
+        #Call function with same job id for another attempt    
+        parse_job_details(job_id, attempt)
+    except Exception as ex:
+        print(f"Exception inside parse_job_details function - {ex}")
+        print(traceback.format_exc())
 
 def write_output(write_mode):
     #writing the list of job_detail object to csv file
@@ -104,11 +113,11 @@ def write_output(write_mode):
         writer = csv.writer(file, delimiter='~')
         if(write_mode == "header"):
             # Write header manually (if needed)
-            writer.writerow(['job_title','company_name', 'company_location', 'pay_range','job_level','employment_type','job_posted_date'])
+            writer.writerow(['job_id','job_title','company_name', 'company_location', 'pay_range','job_level','employment_type','job_posted_date','industry'])
         else:
             # Write job data
             for jd in job_details:
-                writer.writerow([jd.job_title, jd.company_name, jd.company_location, jd.pay_range, jd.job_level, jd.employment_type, jd.job_posted_datetime])
+                writer.writerow([jd.job_id,jd.job_title, jd.company_name, jd.company_location, jd.pay_range, jd.job_level, jd.employment_type, jd.job_posted_datetime,jd.industry])
 
 def process_jobs():
     processed_job_count = 0
@@ -116,7 +125,7 @@ def process_jobs():
     print(f"Found {job_count} jobs which will loop {job_count/10} times")
     start_position = 1
     reattempt = 1
-    while start_position <= (job_count/10) or reattempt < 50:
+    while start_position <= (job_count/10) and reattempt <= 50:
         #jobs_list_url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Data%2BAnalyst&location=United%2BStates&start={start_position}"
         jobs_list_url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search"
 
@@ -136,17 +145,17 @@ def process_jobs():
             reattempt = 1
             jobs_list_soup = BeautifulSoup(job_list_response.text, "html.parser")
             jobs_list = jobs_list_soup.find_all("li")
-            print(f"Found {len(jobs_list)} more jobs in page - {start_position} at {datetime.now()}")
+            print(f"Found {len(jobs_list)} more jobs in page - {start_position} at {datetime.now()}. Processed job count {processed_job_count}")
             for job_detail in jobs_list:
                 try:
-                    base_card_div = job_detail.find("div", {"class": "base-card"})
+                    base_card_div = job_detail.find(attrs={"class": "base-card"})
                     job_id = base_card_div.get("data-entity-urn").split(":")[3]
                     parse_job_details(job_id)
                     processed_job_count += 1
                     #wait 1 second before continuing the loop
                     # time.sleep(1)
                 except Exception as ex:
-                    print(f"Error occured -> {ex}")    
+                    print(f"Error occured -> {ex}")      
         else:
             print(f"Reponse code => {job_list_response.status_code} , Current Start position(page) => {start_position} , attempt -> {reattempt}, time {datetime.now()}")
             start_position -= 1
@@ -155,46 +164,51 @@ def process_jobs():
         start_position += 1
     print(f"Processed jobs - {processed_job_count}")
 
-# start execution
-start_time = datetime.now()
-print(f"Execution started at {start_time}")
-write_output("header")
+if __name__ == "__main__":
+    # start execution
+    start_time = datetime.now()
+    print(f"Execution started at {start_time}")
+    write_output("header")
 
-queryParams = {}
-if(job_title != ""):
-    print(f"Job title search condition is: {job_title}")
-    queryParams["keywords"] = job_title
+    queryParams = {}
+    if(job_title != ""):
+        print(f"Job title search condition is: {job_title}")
+        queryParams["keywords"] = job_title
 
-if(job_location != ""):
-    print(f"Job location search condition is: {job_location}")
-    queryParams["location"] = job_location
+    if(job_location != ""):
+        print(f"Job location search condition is: {job_location}")
+        queryParams["location"] = job_location
 
-queryParams["pageNum"] = 0
+    queryParams["pageNum"] = 0
 
-job_count = 0
+    job_count = 0
 
-#Finding the number of jobs
-tryattempt = 0
-while tryattempt < 10:
-    # initial_url = "https://www.linkedin.com/jobs/search?keywords=Data%20Analyst&location=United%20States&position=1&pageNum=0"
-    initial_url = "https://www.linkedin.com/jobs/search"
-    initial_url_response = requests.get(initial_url, verify=False, params=queryParams)
-    if(initial_url_response.status_code == 200):
-        init_soup  = BeautifulSoup(initial_url_response.text, "html.parser")
-        job_count_txt = init_soup.find("span", {"class", "results-context-header__job-count"}).text
-        #Get only the numbers from job count text
-        job_count = int(re.sub(r"[^0-9]","",job_count_txt))
-        break
-    else:
-        tryattempt += 1
-#Delete pageNum from the query params for other requests after getting the number of jobs
-del queryParams["pageNum"]
+    #Finding the number of jobs
+    tryattempt = 0
+    while tryattempt < 10:
+        # initial_url = "https://www.linkedin.com/jobs/search?keywords=Data%20Analyst&location=United%20States&position=1&pageNum=0"
+        initial_url = "https://www.linkedin.com/jobs/search"
+        initial_url_response = requests.get(initial_url, verify=False, params=queryParams)
+        if(initial_url_response.status_code == 200):
+            init_soup  = BeautifulSoup(initial_url_response.text, "html.parser")
+            job_count_txt = init_soup.find("span", {"class", "results-context-header__job-count"}).text
+            #Get only the numbers from job count text
+            job_count = int(re.sub(r"[^0-9]","",job_count_txt))
+            break
+        else:
+            tryattempt += 1
+    #Delete pageNum from the query params for other requests after getting the number of jobs
+    del queryParams["pageNum"]
 
-try:
-    process_jobs()
-except Exception as ex:
-    print(f"Error occured -> {ex}")
+    try:
+        process_jobs()
+    except Exception as ex:
+        print(f"Error occured -> {ex}")
 
+    #If remaining data in job_details, append to file
+    if(len(job_details)) > 0 :
+        write_output("data")
 
-print(f"Execution completed at {datetime.now()}")
-print(f"Execution completed in {datetime.now() - start_time}")
+    print(f"Execution completed at {datetime.now()}")
+    print(f"Execution completed in {datetime.now() - start_time}")
+
